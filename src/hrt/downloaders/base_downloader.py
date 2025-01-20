b37b8eb340c2e3e4d6453a34edbd2b690184cbe2
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-01-19 20:34:36 -0500

    add base downloader

--- src/hrt/downloaders/base_downloader.py
+++ src/hrt/downloaders/base_downloader.py
@@ -1,6 +1,9 @@
+import os
 from abc import ABC, abstractmethod
 
-from hrt.common.enums import CallSignDownloadType, ExamType
+from hrt.common import utils
+from hrt.common.config_reader import logger
+from hrt.common.enums import CallSignDownloadType, CountryCode, DownloadType, ExamType
 
 
 class IDownloader(ABC):
@@ -23,5 +26,62 @@ class IDownloader(ABC):
         pass
 
     @abstractmethod
+    def post_process(self):
+        pass
+
+
+class BaseDownloader(IDownloader, ABC):
+    def __init__(
+        self,
+        chrome_driver_path: str,
+        country: CountryCode,
+        download_type: DownloadType,
+        output_folder: str,
+        config: dict,
+    ):
+        self._chrome_driver_path = chrome_driver_path
+        self._country = country
+        self._download_type = download_type
+        self._output_folder = output_folder
+        self._config = config
+        self._output_file_path = None
+
+    @property
+    def chrome_driver_path(self):
+        return self._chrome_driver_path
+
+    @property
+    def country(self):
+        return self._country
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def output_folder(self):
+        return self._output_folder
+
+    @property
+    def download_type(self):
+        return self._download_type
+
+    def get_output_folder(self):
+        if not self.output_folder or self.output_folder.strip() == "":
+            logger.error("Output folder not found")
+            return None
+
+        return os.path.join(self.output_folder, self.country.code)
+
+    def download(self, url, output_file_path, zip_files: list[str] = None):
+        self.pre_process()
+        output_folder = os.path.dirname(output_file_path)
+        utils.create_folder(output_folder)
+        utils.download_file(url, output_file_path, zip_files)
+        self.post_process()
+
+    def pre_process(self):
+        pass
+
     def post_process(self):
         pass
