52d109281ae7100f78a61dcf516290a6a4315588
Phani K <192951055+phani-kb@users.noreply.github.com>
2025-01-27 22:36:12 -0500

    update country specific quizzes

--- src/hrt/question_banks/ca_quiz.py
+++ src/hrt/question_banks/ca_quiz.py
@@ -1,7 +1,29 @@
-from abc import ABC
+from hrt.common.enums import ExamType, QuestionDisplayMode, QuizAnswerDisplay
+from hrt.common.question import Question
 
-from hrt.common.quiz import Quiz
 
+class CAQuiz:
+    PASS_PERCENTAGE = 70
+    PASS_PERCENTAGE_WITH_HONOURS = 80
 
-class CAQuiz(Quiz, ABC):
-    pass
+    def pre_process(self):
+        pass
+
+    def post_process(self):
+        pass
+
+    def start(self):
+        pass
+
+    def __init__(
+        self,
+        number_of_questions: int,
+        questions: list[Question],
+        exam_type: ExamType,
+        display_mode: QuestionDisplayMode,
+        answer_display: QuizAnswerDisplay,
+        quiz_config: dict,
+    ):
+        super().__init__(
+            number_of_questions, questions, exam_type, display_mode, answer_display, quiz_config
+        )
--- src/hrt/question_banks/us_quiz.py
+++ src/hrt/question_banks/us_quiz.py
@@ -1,7 +1,26 @@
-from abc import ABC
+from hrt.common.enums import ExamType, QuestionDisplayMode, QuizAnswerDisplay
+from hrt.common.question import Question
 
-from hrt.common.quiz import Quiz
 
+class USQuiz:
+    def pre_process(self):
+        pass
 
-class USQuiz(Quiz, ABC):
-    pass
+    def post_process(self):
+        pass
+
+    def start(self):
+        raise NotImplementedError("USQuiz.start() not supported yet")
+
+    def __init__(
+        self,
+        number_of_questions: int,
+        questions: list[Question],
+        exam_type: ExamType,
+        display_mode: QuestionDisplayMode,
+        answer_display: QuizAnswerDisplay,
+        quiz_config: dict,
+    ):
+        super().__init__(
+            number_of_questions, questions, exam_type, display_mode, answer_display, quiz_config
+        )
